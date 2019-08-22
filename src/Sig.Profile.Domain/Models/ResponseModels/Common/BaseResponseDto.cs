ce7b14fd2539b7d0fbd408a2352c3424d80940b8
sospina <sigifredo.ospina@kaplan.edu>
2019-08-22 13:40:07 -0400

    Updates include  - created CreateProfile + CreateProfileRequestDto, added  Utility class to handle common functionality, removed out-of-box controller ValuesController, Updated launchsettings to include swagger settings

---
+++ src/Sig.Profile.Domain/Models/ResponseModels/Common/BaseResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sig.Profile.Domain.Models.ResponseModels.Interfaces.Common;
+
+namespace Sig.Profile.Domain.Models.ResponseModels.Common
+{
+    public class BaseResponseDto : IResponseDto
+    {
+    }
+}
--- src/Sig.Profile.Service/Startup.cs
+++ src/Sig.Profile.Service/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,24 +9,62 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
+using Serilog;
+using Serilog.Formatting.Json;
+using Swashbuckle.AspNetCore.Swagger;
+
 
 namespace Sig.Profile.Service
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        /// <summary>
+        /// Configuration of IConfiguration
+        /// </summary>
+        public static IConfiguration Configuration { get; set; }
+
+
+        /// <summary>
+        /// For Containter
+        /// </summary>
+        //public IContainer ApplicationContainer { get; private set; } 
+
+
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(env.ContentRootPath)
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional:true)
+                    .AddJsonFile($"sharedsettings.json", optional:true)
+                    .AddJsonFile($"/app/settings/sharedsettings.json", optional:true)
+                    .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            CreateAddLogger(services);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<IConfiguration>(Configuration);
+
+            var assemblies = new[] {
+                Assembly.Load("KT.MobileFunding.Providers"),
+                Assembly.Load("KT.MobileFunding.Business"),
+                Assembly.Load("KT.MobileFunding.Domain")
+            };
+
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +82,22 @@ namespace Sig.Profile.Service
 
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        //nonstandard
+        /// <summary>
+        /// Setting up Logging using Serilog
+        /// </summary>
+        /// <param name="services"></param>
+        private static void CreateAddLogger(IServiceCollection services)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            services.AddLogging();
+            services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
         }
     }
 }
